a8176bc319fd6370d6f2ebed39ccd1f9833c7e66
Michael Carvalho <michaeldougmk@gmail.com>
2023-09-26 18:50:15 -0300

    teste

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className="bg-black flex-auto items-center justif">
-      <Text className="text-white ">Open up App.js to start working on your app!</Text>
+    <View className="bg-black flex-auto items-center justify-center">
+      <Text className="text-white">hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
