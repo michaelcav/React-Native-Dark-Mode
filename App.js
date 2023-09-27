af9fc2d7a8d7bfe582ca78647c44f7b1f5440d05
Michael Carvalho <michaeldougmk@gmail.com>
2023-09-26 21:51:35 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { View, Text, Switch } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "nativewind";
 
 export default function App() {
+  const {colorScheme, toggleColorScheme} = useColorScheme();
   return (
-    <View className="bg-black flex-auto items-center justify-center">
-      <Text className="text-white">hello world</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 justify-center items-center dark:bg-neutral-900">
+      <StatusBar style={colorScheme=='dark' ? 'light':'dark'}/>
+      <View className="flex-row items-center">
+        <Text className="text-xl dark:text-white">
+          Toggle Theme</Text>
+          <Switch value={colorScheme=='dark'} onChange={toggleColorScheme} />
+      </View>
+      <Text className="dark:text-white">Open your mind!</Text>
     </View>
   );
 }
-
